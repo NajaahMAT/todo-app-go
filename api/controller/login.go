0b8d5bc85b9624322218d9166efb54490fa9c2e1
NajaahMAT <najaah9@gmail.com>
2023-06-02 17:27:58 +0530

    login controller implementation

--- api/controller/login.go
+++ api/controller/login.go
@@ -1,1 +1,55 @@
 package controller
+
+import (
+	"net/http"
+	"todo-app-go/bootstrap"
+	"todo-app-go/domain"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type loginController struct {
+	LoginUsecase domain.LoginUsecase
+	Env          *bootstrap.Env
+}
+
+func (l *loginController) Login(c *gin.Context) {
+	var loginRequest domain.LoginRequest
+
+	err := c.ShouldBind(&loginRequest)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	user, err := l.LoginUsecase.GetUserByEmail(c, loginRequest.Email)
+	if err != nil {
+		c.JSON(http.StatusNotFound, domain.ErrorResponse{Message: "User not found with the given email"})
+		return
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(loginRequest.Password)) != nil {
+		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "Invalid credentials"})
+		return
+	}
+
+	accessToken, err := l.LoginUsecase.CreateAccessToken(&user, l.Env.AccessTokenSecret, l.Env.AccessTokenExpiryHour)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	refreshToken, err := l.LoginUsecase.CreateRefreshToken(&user, l.Env.RefreshTokenSecret, l.Env.RefreshTokenExpiryHour)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, domain.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	loginResponse := domain.LoginResponse{
+		AccessToken:  accessToken,
+		RefreshToken: refreshToken,
+	}
+
+	c.JSON(http.StatusOK, loginResponse)
+}
