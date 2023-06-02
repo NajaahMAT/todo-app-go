48bf886c2b8d12a454c55d563c0b34994227abba
NajaahMAT <najaah9@gmail.com>
2023-06-02 22:00:32 +0530

    auth middleware implementation

--- api/middleware/auth_middleware.go
+++ api/middleware/auth_middleware.go
@@ -1,1 +1,38 @@
 package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"todo-app-go/domain"
+	"todo-app-go/internal/util"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JwtAuthMiddleware(secret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.Request.Header.Get("Authorization")
+		t := strings.Split(authHeader, " ")
+		if len(t) == 2 {
+			authToken := t[1]
+			authorized, err := util.IsAuthorized(authToken, secret)
+			if authorized {
+				userID, err := util.ExtractIDFromToken(authToken, secret)
+				if err != nil {
+					c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: err.Error()})
+					c.Abort()
+					return
+				}
+				c.Set("x-user-id", userID)
+				c.Next()
+				return
+			}
+			c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: err.Error()})
+			c.Abort()
+			return
+		}
+		c.JSON(http.StatusUnauthorized, domain.ErrorResponse{Message: "Not authorized"})
+		c.Abort()
+	}
+}
--- api/route/route.go
+++ api/route/route.go
@@ -1,10 +1,10 @@
 package route
 
 import (
-	"fmt"
 	"time"
 
 	// "todo-app-go/api/middleware"
+	"todo-app-go/api/middleware"
 	"todo-app-go/bootstrap"
 	"todo-app-go/mongo"
 
@@ -13,15 +13,13 @@ import (
 
 func Setup(env *bootstrap.Env, timeout time.Duration, db mongo.Database, gin *gin.Engine) {
 	publicRouter := gin.Group("")
+	protectedRouter := gin.Group("")
+	protectedRouter.Use(middleware.JwtAuthMiddleware(env.AccessTokenSecret))
 	// All Public APIs
-	fmt.Println("route.route.go/Setup")
 	NewSignupRouter(env, timeout, db, publicRouter)
 	NewLoginRouter(env, timeout, db, publicRouter)
-	// NewRefreshTokenRouter(env, timeout, db, publicRouter)
+	NewTokenRouter(env, timeout, db, publicRouter)
 
-	// protectedRouter := gin.Group("")
-	// // Middleware to verify AccessToken
-	// protectedRouter.Use(middleware.JwtAuthMiddleware(env.AccessTokenSecret))
 	// // All Private APIs
 	// NewProfileRouter(env, timeout, db, protectedRouter)
 	// NewTaskRouter(env, timeout, db, protectedRouter)
