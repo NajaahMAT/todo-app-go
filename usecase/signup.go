a1ac1e61f0fb17f002f17aa9fc96ddccd269d7ca
NajaahMAT <najaah9@gmail.com>
2023-06-02 14:54:55 +0530

    signup usecase implementation

--- usecase/signup.go
+++ usecase/signup.go
@@ -1,1 +1,40 @@
 package usecase
+
+import (
+	"context"
+	"time"
+	"todo-app-go/domain"
+	"todo-app-go/internal/util"
+)
+
+type signupUsecase struct {
+	userRepository domain.UserRepository
+	contextTimeout time.Duration
+}
+
+func NewSignupUsecase(userRepository domain.UserRepository, timeout time.Duration) domain.SignupUsecase {
+	return &signupUsecase{
+		userRepository: userRepository,
+		contextTimeout: timeout,
+	}
+}
+
+func (s *signupUsecase) Create(c context.Context, user *domain.User) error {
+	ctx, cancel := context.WithTimeout(c, s.contextTimeout)
+	defer cancel()
+	return s.userRepository.Create(ctx, user)
+}
+
+func (s *signupUsecase) GetUserByEmail(c context.Context, email string) (domain.User, error) {
+	ctx, cancel := context.WithTimeout(c, s.contextTimeout)
+	defer cancel()
+	return s.userRepository.GetByEmail(ctx, email)
+}
+
+func (s *signupUsecase) CreateAccessToken(user *domain.User, secret string, expiry int) (accessToken string, err error) {
+	return util.CreateAccessToken(user, secret, expiry)
+}
+
+func (s *signupUsecase) CreateRefreshToken(user *domain.User, secret string, expiry int) (refreshToken string, err error) {
+	return util.CreateRefreshToken(user, secret, expiry)
+}
